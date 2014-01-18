b8c074bca32bf4fa8004eeba8d70d2a286ce146f
pikesley <github@orgraphone.org>
2014-01-18 16:20:00 +0000

    ontrack-specific

--- features/step_definitions/get_steps.rb
+++ features/step_definitions/get_steps.rb
@@ -12,6 +12,26 @@ Given(/^it has a value of:$/) do |value|
   @metric.save
 end
 
+Given(/^it has a category of "(.*?)"$/) do |category|
+  @metric.category = category
+  @metric.save
+end
+
+Given(/^it has a subtype of "(.*?)"$/) do |subtype|
+  @metric.subtype = subtype
+  @metric.save
+end
+
+Given(/^it has a value of "(.*?)"$/) do |value|
+  @metric.value = value
+  @metric.save
+end
+
+Given(/^it has a datetime of "(.*?)"$/) do |datetime|
+  @metric.datetime = datetime
+  @metric.save
+end
+
 Then(/^the JSON response should have "(.*?)" with the text:$/) do |arg1, string|
   pending # express the regexp above with the code you wish you had
 end
--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -1,6 +1,6 @@
 Then(/^the data should be stored in the "(.*?)" metric$/) do |metric_name|
-  @metric = Metric.where(name: metric_name).last
-  @metric.name.should == metric_name
+  @metric = Metric.where(type: metric_name).last
+  @metric.type.should == metric_name
 end
 
 Then(/^the time of the stored metric should be "(.*?)"$/) do |time|
@@ -9,4 +9,24 @@ end
 
 Then(/^the value of the metric should be:$/) do |string|
   @metric.value.to_json.should == string
+end
+
+Then(/^the content of the metric should be:$/) do |string|
+  @metric.content.to_json.should == string
+end
+
+Then(/^the datetime of the stored metric should be "(.*?)"$/) do |time|
+  @metric.datetime.should == DateTime.parse(time)
+end
+
+Then(/^the category of the metric should be "(.*?)"$/) do |category|
+  @metric.category.should == category
+end
+
+Then(/^the value of the metric should be "(.*?)"$/) do |value|
+  @metric.value.should == value
+end
+
+Then(/^the subtype of the metric should be "(.*?)"$/) do |subtype|
+  @metric.subtype.should == subtype
 end
