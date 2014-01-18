b4634aceea79e0462425c9f61a564089719cafe2
pikesley <github@orgraphone.org>
2014-01-18 16:20:09 +0000

    ontrack-specific

--- lib/models/metrics.rb
+++ lib/models/metrics.rb
@@ -2,9 +2,11 @@ require 'mongoid'
 
 class Metric
   include Mongoid::Document
-  
-  field :name,  type: String
-  field :time,  type: DateTime
+
+  field :type, type: String
+  field :subtype, type: String
+  field :datetime, type: DateTime
+  field :category, type: String
   field :value
-  
+
 end
